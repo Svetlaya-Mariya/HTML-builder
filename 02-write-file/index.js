e09c6670102a031ea9f605d0682665f7af102998
DESKTOP-LKHU9F7\merry <merry76light@gmail.com>
2022-05-23 23:58:00 +0300

    feat: add 02-write-file task

---
+++ 02-write-file/index.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+const {stdin} = require('process');
+
+const rl = readline.createInterface(stdin);
+
+const stream = fs.createWriteStream(
+    path.join(__dirname, 'text.txt'),
+    'utf-8',
+    err => {
+        if (err) {
+            console.log('Error', err.message)
+        }
+    }
+);
+console.log('Hello. Write some text');
+
+rl.on('line', (data) => {
+    if (data.toString() === 'exit'){
+        console.log('Ok. Your text write to file "text.txt".');
+        process.exit();
+    }
+    else {
+        stream.write(data.toString() + '\n');
+    }
+});
+
+process.on('SIGINT', () => {
+    console.log('Ok. Your text write to file "text.txt".');
+    process.exit();
+})
